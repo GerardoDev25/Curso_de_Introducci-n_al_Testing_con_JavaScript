240d4f721ef780ac3d472d5949dff58d319bd956
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-07 12:36:42 -0400

    Setup and Teardown

--- demos/src/test/05-setup.test.js
+++ demos/src/test/05-setup.test.js
@@ -1,5 +1,36 @@
 describe('set', () => {
+  beforeAll(() => {
+    console.log('beforeAll');
+  });
+
+  beforeEach(() => {
+    console.log('beforeEach');
+  });
+
+  afterAll(() => {
+    console.log('afterAll');
+  });
+
+  afterEach(() => {
+    console.log('afterEach');
+  });
+
   test('case 1', () => {
-    expect();
+    expect(1 + 1).toBe(2);
+  });
+
+  test('case 1.1', () => {
+    expect(1 + 1).toBe(2);
+  });
+  describe('set', () => {
+    test('case 2', () => {
+      expect(1 + 2).toBe(3);
+    });
+
+    describe('set', () => {
+      test('case 3', () => {
+        expect(1 + 3).toBe(4);
+      });
+    });
   });
 });
