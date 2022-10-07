1955e434e86876798d614d0c2e50996c242ff503
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-07 10:24:28 -0400

    Pruebas estáticas

--- demos/src/test/02-math.test.js
+++ demos/src/test/02-math.test.js
@@ -1,25 +1,23 @@
-const { divide, multiply, sum } = require("../02-math");
+const { divide, multiply, sum } = require('../02-math');
 
-describe("test in 02-math", () => {
-  test("add 1 + 3 shuold be 4", () => {
+describe('test in 02-math', () => {
+  test('add 1 + 3 shuold be 4', () => {
     const res = sum(1, 3);
     expect(res).toBe(4);
   });
 
-  test("shuold be 4", () => {
+  test('shuold be 4', () => {
     const res = multiply(2, 2);
     expect(res).toBe(4);
   });
 
-  test("shuold be 1", () => {
+  test('shuold be 1', () => {
     const res = divide(2, 2);
     expect(res).toBe(1);
   });
 
-  test("shuold be null", () => {
+  test('shuold be null', () => {
     const res = divide(2, 0);
     expect(res).toBeNull();
   });
-
-
 });
