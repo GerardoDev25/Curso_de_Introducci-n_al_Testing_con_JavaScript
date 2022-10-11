00df55003ae6e55f752c87f798ca7c0c516ed592
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-11 10:05:29 -0400

    Mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,10 +1,36 @@
+/* eslint-disable comma-dangle */
 const BookService = require('./books.service');
+
+const fakebooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+  {
+    _id: 2,
+    name: 'bla blabla',
+  },
+];
+
+const MongoLib = {
+  getAll: () => [...fakebooks],
+  create: () => {},
+};
+
+jest.mock(
+  '../lib/mongo.lib.js',
+  () =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    jest.fn().mockImplementation(() => MongoLib)
+  // eslint-disable-next-line function-paren-newline
+);
 
 describe('test for booksService', () => {
   let service;
 
   beforeEach(() => {
     service = new BookService();
+    jest.clearAllMocks();
   });
 
   describe('test for getBooks', () => {
